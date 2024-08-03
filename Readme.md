31578c89f02122877cbcc5412fce4c489cb649c0
PrateekRajput12 <ficredinn@gmail.com>
2024-08-03 13:02:04 +0530

    Lec-2 Done

---
+++ Readme.md
@@ -0,0 +1,10 @@
+
+
+
+Lec-2 IGNITNG THE APP
+- Setup
+       - npm init
+       -npm install -D parcel
+       -npm install react
+       -npm install react-dom
+       -npx parcel index.html
--- script.js
+++ script.js
@@ -1,13 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-
-// const h= React.createElement("h2",{className:"heading"},"Namaste React")
 
 const root =ReactDOM.createRoot(document.getElementById("root"))
 
-// const main= React.createElement("div",{className:"main"},[
-//     React.createElement("h1",{className:"child 1"},"This is Sibling 1"),
-//     React.createElement("p",{className:"child 2"},"This is Sibling 2"),
-//     ])
 
 
     const main= React.createElement("div",{className:"main"},
